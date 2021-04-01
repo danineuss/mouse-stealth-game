df671a44b6e636947fb3ae5ea82457a3be1c3279
Dani Singer <daniel.singer.ch@gmail.com>
2021-04-01 14:56:58 +0200

    VisionCone: FOV and Range working nicely now.

--- Assets/Scripts/VisionConeController.cs
+++ Assets/Scripts/VisionConeController.cs
@@ -7,9 +7,12 @@ using UnityEngine.Assertions;
 public class VisionConeController : MonoBehaviour
 {
     public float FieldOfView = 50;
+    public float Range = 10;
     public float TiltAngle = 40;
     public float PanAngle = 0;
-    private Transform panParent, tiltParent, cone;
+    public Transform Player;
+
+    private Transform panParent, tiltParent, scaleParent;
     private Light spotLight;
 
     void Start()
@@ -20,22 +23,31 @@ public class VisionConeController : MonoBehaviour
         // transform.LookAt(target);
         // float angle = (TiltAngle + 90) * Mathf.Deg2Rad;
         // Vector3 rayDirection = transform.forward * Mathf.Sin(angle) + transform.right * Mathf.Cos(angle);
-        // Debug.DrawRay(transform.position, transform.forward, Color.red, 3f);
+        Debug.DrawLine(tiltParent.position, tiltParent.position + tiltParent.right * 7, Color.red, 3f);
         // Debug.DrawRay(transform.position, transform.up, Color.green, 3f);
         // Debug.DrawRay(transform.position, transform.right, Color.blue, 3f);
         // Debug.DrawLine(transform.position, transform.position + rayDirection * 7, Color.red, 3f);
     }
 
+    void Update()
+    {
+        UpdateVisionConeOrientation();
+
+        float angleToPlayer = Vector3.Angle(tiltParent.right, Player.transform.position - transform.position);
+        
+    }
+
     void InitializeComponents() {
+        spotLight = GetComponentsInChildren<Light>().Where(x => x.name == "Spot Light").First();
         panParent = GetComponentsInChildren<Transform>().Where(x => x.name == "Pan Parent").First();
         tiltParent = GetComponentsInChildren<Transform>().Where(x => x.name == "Tilt Parent").First();
-        spotLight = GetComponentsInChildren<Light>().Where(x => x.name == "Spot Light").First();
-        cone = GetComponentsInChildren<Transform>().Where(x => x.name == "Cone").First();
+        scaleParent = GetComponentsInChildren<Transform>().Where(x => x.name == "Scale Parent").First();
 
+        Assert.IsNotNull(Player, "Player is null.");
+        Assert.IsNotNull(spotLight, "Spot light is null.");
         Assert.IsNotNull(panParent, "Pan parent is null.");
         Assert.IsNotNull(tiltParent, "Tilt parent is null.");
-        Assert.IsNotNull(spotLight, "Spot light is null.");
-        Assert.IsNotNull(cone, "Cone is null.");
+        Assert.IsNotNull(scaleParent, "Scale Parent is null.");
     }
 
     void UpdateVisionConeOrientation() {
@@ -43,14 +55,9 @@ public class VisionConeController : MonoBehaviour
         tiltParent.localEulerAngles = new Vector3(0, TiltAngle, 0);
 
         spotLight.spotAngle = FieldOfView;
+        spotLight.range = Range;
 
-        var currentScaleZ = cone.transform.localScale.z;
-        var newScaleXY = 2 * currentScaleZ * Mathf.Tan(FieldOfView / 2 * Mathf.Deg2Rad);
-        cone.transform.localScale = new Vector3(newScaleXY, newScaleXY, currentScaleZ);
-    }
-
-    void Update()
-    {
-        UpdateVisionConeOrientation();
+        var newScaleYZ = 2 * Range / 6 * Mathf.Tan(FieldOfView / 2 * Mathf.Deg2Rad);
+        scaleParent.localScale = new Vector3(Range / 6, newScaleYZ, newScaleYZ);
     }
 }
