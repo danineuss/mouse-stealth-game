6119d84e86ffc2d36a13e7735bd62ff7a6e55257
Dani Singer <daniel.singer.ch@gmail.com>
2021-04-07 12:05:58 +0200

    Gizmos: visualize vision control points.

---
+++ Assets/Scripts/VisionConeGizmoVisualizer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class VisionConeGizmoVisualizer : MonoBehaviour
+{
+    public List<VisionControlPoint> visionControlPoints;
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, 0.25f);
+        if (visionControlPoints.Count == 0) { return; }
+
+        foreach (var controlPoint in visionControlPoints) {
+            DrawGizmosForControlPoint(controlPoint);
+        }
+    }
+
+    private void DrawGizmosForControlPoint(VisionControlPoint controlPoint) {
+        Vector3 positionControlPoint = controlPoint.transform.position;
+        var radius = (positionControlPoint - transform.position).magnitude * Mathf.Tan(controlPoint.FieldOfView / 2 * Mathf.Deg2Rad);
+
+        Gizmos.DrawLine(transform.position, positionControlPoint);
+        Gizmos.DrawWireSphere(positionControlPoint, radius);
+    }
+}
---
+++ Assets/Scripts/VisionControlPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionControlPoint : MonoBehaviour
+{
+    [SerializeField] private float fieldOfView; 
+    public float FieldOfView {
+        get { return fieldOfView; }
+        private set { fieldOfView = value; }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+    }
+}
