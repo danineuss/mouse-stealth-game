c281596a7dc06b1df759950e97eaa6c5e7bf78ad
Dani Singer <daniel.singer.ch@gmail.com>
2021-04-07 16:57:12 +0200

    VisionCone: moved detection logic to PlayerDetector.

---
+++ Assets/Scripts/PlayerDetector.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DetectorState {
+    Idle,
+    Searching,
+    Alarmed,
+    Distracted
+}
+
+public class PlayerDetector : MonoBehaviour
+{
+    [SerializeField] private Transform Player;
+    [SerializeField] private LayerMask ObstacleMask;
+    public DetectorState CurrentDetectorState { get; private set; }
+    private VisionCone visionCone;
+
+    void Start()
+    {
+        visionCone = GetComponentInChildren<VisionCone>();
+        CurrentDetectorState = DetectorState.Idle;
+
+        IEnumerator detectPlayerCoroutine = DetectPlayerWithDelay(0.2f);
+        StartCoroutine(detectPlayerCoroutine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DetectPlayerWithDelay(float seconds) {
+        while (true) {
+            yield return new WaitForSeconds(seconds);
+            DetectPlayer();
+            visionCone.SetSpotState(CurrentDetectorState);
+        }
+    }
+
+    void DetectPlayer() {
+        float angleToPlayer = Vector3.Angle(visionCone.CurrentLookatTarget - transform.position, 
+                                            Player.transform.position - transform.position);
+        float distanceToPlayer = Vector3.Distance(Player.transform.position, transform.position);
+        
+        if (angleToPlayer > visionCone.FieldOfView / 2 || distanceToPlayer > visionCone.Range) {
+            CurrentDetectorState = DetectorState.Idle;
+            return;
+        } 
+        
+        bool playerObstructed = Physics.Raycast(
+            transform.position, 
+            (Player.transform.position - transform.position).normalized, 
+            distanceToPlayer, 
+            ObstacleMask
+        );
+        if (playerObstructed) {
+            CurrentDetectorState = DetectorState.Idle;
+            return;
+        }
+
+        CurrentDetectorState = DetectorState.Alarmed;
+    }
+}
---
+++ Assets/Scripts/VisionCone/VisionCone.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class VisionCone : MonoBehaviour
+{
+    [SerializeField] private Transform ConeScaleParent;
+    [SerializeField] private VisionConeControlPoints VisionConeControlPoints;
+    [SerializeField] private Light spotLight;
+    [SerializeField] private float VisionConePeriod = 5f;
+    [SerializeField] private Color kSpotLightGreen = new Color(0f, 183f, 18f, 1f);
+    [SerializeField] private Color kSpotLightOrange = new Color(183f, 102f, 0f, 1f);
+    [SerializeField] private Color kSpotLightRed = new Color(191, 0f, 10f, 1f);
+    [SerializeField] private Color kSpotLightBlue = new Color(0f, 23f, 183f, 1f);
+
+    public Vector3 CurrentLookatTarget { get; private set; }
+    public float FieldOfView { get; private set; }
+    public float Range { get; private set; }
+    
+    private int controlPointIndex = 0;
+    private VisionConeControlPoint targetControlPoint;
+    private float kConeRangeMultiplier = 1.5f;
+
+    void Start()
+    {
+        InitializeCone();        
+        UpdateVisionConeOrientation();
+        SetNextControlPoint();
+    }
+
+    void Update()
+    {
+        UpdateVisionConeOrientation();
+    }
+
+    void InitializeCone() {
+        var currentControlPoint = VisionConeControlPoints.controlPoints[controlPointIndex];
+        CurrentLookatTarget = currentControlPoint.transform.position;
+        FieldOfView = currentControlPoint.FieldOfView;
+    }
+
+    void UpdateVisionConeOrientation() {
+        var deltaVector = CurrentLookatTarget - transform.position;
+        Range = deltaVector.magnitude * kConeRangeMultiplier;
+        transform.rotation = Quaternion.LookRotation(deltaVector);
+
+        spotLight.spotAngle = FieldOfView;
+        spotLight.range = Range;
+
+        // Some of the following numbers are the result of the scale of the Cone 3D Model.
+        var newScaleXY = 2 * Range / 6 * Mathf.Tan(FieldOfView / 2 * Mathf.Deg2Rad);
+        ConeScaleParent.localScale = new Vector3(newScaleXY, newScaleXY, Range / 6);
+    }
+
+    void SetNextControlPoint() {
+        controlPointIndex = 1 - controlPointIndex;
+        var newControlPoint = VisionConeControlPoints.controlPoints[controlPointIndex];
+        var newTarget = newControlPoint.transform.position;
+        var newFieldOfView = newControlPoint.FieldOfView;
+        IEnumerator lerpLookatTarget = LerpLookatTarget(newTarget, newFieldOfView, VisionConePeriod / 2);
+        StartCoroutine(lerpLookatTarget);
+    }
+
+    IEnumerator LerpLookatTarget(Vector3 newLookatTarget, float newFieldOfView, float durationSeconds) {
+        float elapsedTime = 0f;
+        float startFieldOfView = FieldOfView;
+        Vector3 startLookatTarget = CurrentLookatTarget;
+        while (elapsedTime < durationSeconds) {
+            var t = elapsedTime / durationSeconds;
+            FieldOfView = Mathf.Lerp(startFieldOfView, newFieldOfView, t);
+            CurrentLookatTarget = Vector3.Lerp(startLookatTarget, newLookatTarget, t);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        SetNextControlPoint();
+    }
+
+    public void SetSpotState(DetectorState newDetectorState) {
+        switch (newDetectorState) {
+            case DetectorState.Idle:
+                spotLight.color = kSpotLightGreen;
+                break;
+            case DetectorState.Searching:
+                spotLight.color = kSpotLightOrange;
+                break;
+            case DetectorState.Alarmed:
+                spotLight.color = kSpotLightRed;
+                break;
+            case DetectorState.Distracted:
+                spotLight.color = kSpotLightBlue;
+                break;
+        }
+
+    }
+}
