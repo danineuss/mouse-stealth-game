fad8a88148edaf62b461e742d80e6f89167e62e8
Dani Singer <daniel.singer.ch@gmail.com>
2021-04-07 15:36:29 +0200

    VisionCone: can now patrol between control points.

--- Assets/Scripts/VisionCone.cs
+++ Assets/Scripts/VisionCone.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 public class VisionCone : MonoBehaviour
 {
@@ -11,10 +10,13 @@ public class VisionCone : MonoBehaviour
     public Transform ConeScaleParent;
     public VisionConeControlPoints VisionConeControlPoints;
     public Light spotLight;
+    public float VisionConePeriod = 5f;
 
-    private Vector3 currentTarget;
-    private float fieldOfView = 50;
-    private float range = 10;
+    private Vector3 currentLookatTarget;
+    private float fieldOfView;
+    private float range;
+    private int controlPointIndex = 0;
+    private VisionConeControlPoint targetControlPoint;
     private Color kSpotLightGreen = new Color(0f, 183f, 18f, 1f);
     private float kConeRangeMultiplier = 1.5f;
 
@@ -22,6 +24,7 @@ public class VisionCone : MonoBehaviour
     {
         InitializeCone();        
         UpdateVisionConeOrientation();
+        SetNextControlPoint();
 
         IEnumerator detectPlayerCoroutine = DetectPlayerWithDelay(0.2f);
         StartCoroutine(detectPlayerCoroutine);
@@ -33,21 +36,45 @@ public class VisionCone : MonoBehaviour
     }
 
     void InitializeCone() {
-        var currentControlPoint = VisionConeControlPoints.controlPoints.FirstOrDefault();
-        currentTarget = currentControlPoint.transform.position;
+        var currentControlPoint = VisionConeControlPoints.controlPoints[controlPointIndex];
+        currentLookatTarget = currentControlPoint.transform.position;
         fieldOfView = currentControlPoint.FieldOfView;
     }
 
     void UpdateVisionConeOrientation() {
-        var deltaVector = currentTarget - transform.position;
+        var deltaVector = currentLookatTarget - transform.position;
         range = deltaVector.magnitude * kConeRangeMultiplier;
         transform.rotation = Quaternion.LookRotation(deltaVector);
 
         spotLight.spotAngle = fieldOfView;
         spotLight.range = range;
 
+        // Some of the following numbers are the result of the scale of the Cone 3D Model.
         var newScaleXY = 2 * range / 6 * Mathf.Tan(fieldOfView / 2 * Mathf.Deg2Rad);
         ConeScaleParent.localScale = new Vector3(newScaleXY, newScaleXY, range / 6);
+    }
+
+    void SetNextControlPoint() {
+        controlPointIndex = 1 - controlPointIndex;
+        var newControlPoint = VisionConeControlPoints.controlPoints[controlPointIndex];
+        var newTarget = newControlPoint.transform.position;
+        var newFieldOfView = newControlPoint.FieldOfView;
+        IEnumerator lerpLookatTarget = LerpLookatTarget(newTarget, newFieldOfView, VisionConePeriod / 2);
+        StartCoroutine(lerpLookatTarget);
+    }
+
+    IEnumerator LerpLookatTarget(Vector3 newLookatTarget, float newFieldOfView, float durationSeconds) {
+        float elapsedTime = 0f;
+        float startFieldOfView = fieldOfView;
+        Vector3 startLookatTarget = currentLookatTarget;
+        while (elapsedTime < durationSeconds) {
+            var t = elapsedTime / durationSeconds;
+            fieldOfView = Mathf.Lerp(startFieldOfView, newFieldOfView, t);
+            currentLookatTarget = Vector3.Lerp(startLookatTarget, newLookatTarget, t);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        SetNextControlPoint();
     }
 
     IEnumerator DetectPlayerWithDelay(float seconds) {
