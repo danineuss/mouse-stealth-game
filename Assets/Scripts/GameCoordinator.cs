94fc9f9faeec5b8600db2bc3bc874bbae7b8c5d8
Dani Singer <daniel.singer.ch@gmail.com>
2021-04-09 10:54:19 +0200

    GameCoordinator: mission failed screen working.

---
+++ Assets/Scripts/GameCoordinator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState {
+    Idle,
+    Paused,
+    Failed
+}
+
+public class GameCoordinator : MonoBehaviour
+{
+    public List<PlayerDetector> PlayerDetectors;
+    public UICoordinator UICoordinator;
+
+    private GameState gameState = GameState.Idle;
+
+    void Update()
+    {
+        CheckPlayerDetection();
+        CheckGamePaused();
+        UpdateUI();
+    }
+
+    void CheckPlayerDetection() {
+        foreach (var playerDetector in PlayerDetectors) {
+            if (playerDetector.CurrentDetectorState.Equals(DetectorState.Alarmed)) {
+                gameState = GameState.Failed;
+                Time.timeScale = 0.0f;
+                return;
+            }
+        }
+    }
+
+    void CheckGamePaused() {
+
+    }
+
+    void UpdateUI() {
+        switch (gameState)
+        {
+            case GameState.Idle:
+                UICoordinator.HideGamePaused();
+                break;
+            case GameState.Paused:
+                UICoordinator.ShowGamePaused();
+                break;
+            case GameState.Failed:
+                UICoordinator.ShowGameFailed();
+                break;
+            default:
+                throw new InvalidOperationException("Switch case not exhaustive, code shoud not reach here.");
+        }
+    }
+}
---
+++ Assets/Scripts/UICoordinator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class UICoordinator : MonoBehaviour
+{
+    private GameObject failedScreen;
+
+    void Start() {
+        failedScreen = GetComponentsInChildren<Transform>()
+                        .Where(x => x.CompareTag("FailedScreen"))
+                        .First()
+                        .gameObject;
+        failedScreen.SetActive(false);
+    }
+
+    public void HideGamePaused() {
+        
+    }
+
+    public void ShowGamePaused() {
+
+    }
+
+    public void ShowGameFailed() {
+        failedScreen.SetActive(true);
+    }
+}
