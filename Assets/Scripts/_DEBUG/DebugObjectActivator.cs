88ca5af48f8b4f9649aa4f7ae1f0d1da26c3ba05
Dani Singer <daniel.singer.ch@gmail.com>
2021-04-29 11:57:53 +0200

    Debug: activate Person Door at Door Trigger.

---
+++ Assets/Scripts/_DEBUG/DebugObjectActivator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugObjectActivator : MonoBehaviour {
+    public GameObject Object;
+    public bool DesiredStateAfterTrigger;
+    
+    void OnTriggerEnter() {
+        Object.SetActive(DesiredStateAfterTrigger);
+    }
+}
