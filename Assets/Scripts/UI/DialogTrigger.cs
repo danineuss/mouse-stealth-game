7ba483f8730c3cdef59c3fc8d4c553b6b852cd9d
Dani Singer <daniel.singer.ch@gmail.com>
2021-05-03 12:18:52 +0200

    Scene: a bunch of fixes.
    Fixes:
    . free music
    . dialog trigger
    . wall and door mats
    . better lighting + door spots
    . Person Door fixed view cone

--- Assets/Scripts/UI/DialogTrigger.cs
+++ Assets/Scripts/UI/DialogTrigger.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DialogTrigger : MonoBehaviour {
+    private Collider triggerCollider;
     [SerializeField] private DialogVM dialogVM;
     [SerializeField] private SceneVM sceneVM;
     
+    void Awake() {
+        triggerCollider = GetComponent<Collider>();
+    }
+
     void OnTriggerEnter() {
         sceneVM.SceneEvents.DialogOpened(dialogVM);
+        triggerCollider.gameObject.SetActive(false);
     }
 }
