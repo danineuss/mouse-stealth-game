1c20328e0142e5f4e97d4065b73c0c6153d03fdb
danineuss <daniel.neuss@hey.com>
2021-06-10 12:41:48 +0200

    MeshGenerator: vertices looking good.

--- Assets/Scripts/Enemies/VisionCone/VisionConeMeshGeneratorMono.cs
+++ Assets/Scripts/Enemies/VisionCone/VisionConeMeshGeneratorMono.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class VisionConeMeshGeneratorMono : MonoBehaviour
 {
+    [Range(0.1f, 20)] public float Radius;
+    [Range(2, 100)] public int MeshResolution;
+
     [SerializeField] private VisionConeMono visionConeMono = null;
 
     private Mesh mesh;
@@ -18,58 +21,125 @@ public class VisionConeMeshGeneratorMono : MonoBehaviour
 
     void Start()
     {
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
-
         MakeMesh();
     }
 
     public void MakeMesh()
     {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
         CreateMesh();
         UpdateMesh();
-
-        var a = TrianglesFromHexagons(4);
     }
 
     void CreateMesh()
     {
         Vector3 startPoint = visionConeMono.ControlPointsMono.PatrolPoints[0].Position - transform.position;
 
-        vertices = new Vector3[]
-        {
-            new Vector3 (0, 0, 0),
-            new Vector3 (0, 1, 0),
-            new Vector3 (1, 0, 0)
-        }
-            .Select(x => x + startPoint)
-            .ToArray();
+        int numberOfHexagons = Mathf.RoundToInt(Radius / MeshResolution);
+        vertices = VerticesForHexagons(numberOfHexagons);
+        triangles = TrianglesForHexagons(numberOfHexagons);
+    }
 
-        triangles = new int[]
-        {
-            0, 1, 2
+    private Vector3[] VerticesForHexagons(int numberOfHexagonRings)
+    {
+        int numberOfVertices = 1 + Enumerable.Range(1, numberOfHexagonRings)
+            .Select(x => 6 * x)
+            .Sum();
+
+        var vertices = new Vector3[numberOfVertices];
+        vertices[0] = new Vector3(0, 0, 0);
+        foreach (int depth in Enumerable.Range(1, numberOfHexagonRings))
+            vertices = AddVerticesForDepth(vertices, depth);
+
+        return vertices;
+    }
+
+    private Vector3[] AddVerticesForDepth(Vector3[] vertices, int depth)
+    {
+        Vector3[] currentVertices = VerticesForDepth(CornerVerticesForDepth(depth), depth);
+
+        int startingIndex = 1 + Enumerable.Range(1, depth - 1)
+            .Select(x => 6 * x)
+            .Sum();
+        int endIndex = startingIndex + currentVertices.Count();
+        for (int i = startingIndex; i < endIndex; i++)
+            vertices[i] = currentVertices[i - startingIndex];
+
+        return vertices;
+    }
+
+    private Vector3[] CornerVerticesForDepth(int depth)
+    {
+        float outerRadius = Radius / (float)MeshResolution * (float)depth;
+        float innerRadius = outerRadius * 0.866025404f;
+        return new Vector3[] {
+            new Vector3(0f, outerRadius, 0f),
+            new Vector3(-innerRadius, 0.5f * outerRadius, 0f),
+            new Vector3(-innerRadius, -0.5f * outerRadius, 0f),
+            new Vector3(0f, -outerRadius, 0f),
+            new Vector3(innerRadius, -0.5f * outerRadius, 0f),
+            new Vector3(innerRadius, 0.5f * outerRadius, 0f),
         };
     }
 
-    int[] TrianglesFromHexagons(int numberOfRings)
+    private Vector3[] VerticesForDepth(Vector3[] cornerVertices, int depth)
     {
-        List<int> triangles = new List<int>();
-        triangles.AddRange(TrianglesInnerMostHexagon());
-
-        for (int i = 2; i <= numberOfRings; i++)
-            triangles.AddRange(TrianglesFromHexagon(i));
+        if (depth < 2)
+            return cornerVertices;
         
-        return triangles.ToArray();
+        var vertices = new Vector3[cornerVertices.Count() + 6 * (depth - 1)];
+        for(int cornerIndex = 0; cornerIndex < cornerVertices.Count(); cornerIndex++)
+        {
+            Vector3 currentCorner = cornerVertices[cornerIndex];
+            Vector3 nextCorner = cornerVertices[(cornerIndex + 1) % cornerVertices.Count()];
+            vertices[cornerIndex * depth] = currentCorner;
+            for (int i = 1; i < depth; i++)
+                vertices[cornerIndex * depth + i] = currentCorner + i / (float)depth * (nextCorner - currentCorner);
+        }
+
+        return vertices;
     }
 
-    int[] TrianglesFromHexagon(int depthOfRing)
+    private int[] TrianglesForHexagons(int numberOfHexagonRings)
+    {
+        int[] indicesPerDepth = NumberOfIndicesPerDepth(numberOfHexagonRings);
+        var triangles = new int[indicesPerDepth.Sum()];
+
+        int trianglesStartIndex = 0;
+        foreach (int depth in Enumerable.Range(1, numberOfHexagonRings))
+        {
+            int[] currentTriangles;
+            if (depth == 1)
+                currentTriangles = TrianglesInnerMostHexagon();
+            else
+                currentTriangles = TrianglesForHexagon(depth);
+            
+            for (int i = 0; i < currentTriangles.Count(); i++)
+                triangles[trianglesStartIndex + i] = currentTriangles[i];
+            
+            trianglesStartIndex += currentTriangles.Count();
+        }
+        
+        return triangles;
+    }
+
+    private int[] NumberOfIndicesPerDepth(int numberOfHexagonRings)
+    {
+        return Enumerable.Range(1, numberOfHexagonRings)
+            .Select(x => 36 * x - 18)
+            .ToArray();
+    }
+
+    private int[] TrianglesForHexagon(int depthOfRing)
     {
         if (depthOfRing < 2)
             throw new ArgumentException("Triangle calculations only supported with depth >= 2.");
 
         innerPoints = PointsForDepth(depthOfRing - 1);
+        innerCornerPoints = CornerPointsForDepth(depthOfRing - 1);
         outerPoints = PointsForDepth(depthOfRing);
-        innerCornerPoints = InnerCornerPointsForDepth(depthOfRing);
 
         int numberOfEdgePoints = innerPoints.Count() - innerCornerPoints.Count();
         var triangles = new int[3 * (innerCornerPoints.Count() * 3 + numberOfEdgePoints * 2)];
@@ -124,16 +194,16 @@ public class VisionConeMeshGeneratorMono : MonoBehaviour
         return outerPoints;
     }
 
-    private int[] InnerCornerPointsForDepth(int depthOfRing)
+    private int[] CornerPointsForDepth(int depthOfRing)
     {
         int[] cornerPoints = new int[6];
         for (int i = 0; i < cornerPoints.Count(); i++)
-            cornerPoints[i] = 6 * ((depthOfRing - 1) * (depthOfRing - 2) / 2) + 1 + (depthOfRing - 1) * i;
+            cornerPoints[i] = 6 * (depthOfRing * (depthOfRing - 1) / 2) + 1 + depthOfRing * i;
         
         return cornerPoints;
     }
 
-    private static IEnumerable<int> TrianglesInnerMostHexagon()
+    private static int[] TrianglesInnerMostHexagon()
     {
         return new int[]
         {
